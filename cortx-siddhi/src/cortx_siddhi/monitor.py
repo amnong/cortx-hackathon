8abf0b54890c3e003e2027265b35330f483c26ec
Amnon <amnon@weka.io>
2020-12-10 19:11:20 +0000

    fixed syntax errors

--- cortx-siddhi/src/cortx_siddhi/monitor.py
+++ cortx-siddhi/src/cortx_siddhi/monitor.py
@@ -9,7 +9,7 @@ SCAN_INTERVAL = 1 * 60
 SCAN_INTERVAL = 5
 
 class S3Monitor():
-    def __init__():
+    def __init__(self):
         self._cache = {}
 
     @staticmethod
@@ -22,19 +22,19 @@ class S3Monitor():
         result = set((bucket['Name'], bucket['CreationDate']) for bucket in query_result)
         return result
 
-    def monitor_buckets(siddhi_input_handler):
+    def monitor_buckets(self, siddhi_input_handler):
         client = config.get_client()
-        previous_buckets = get_buckets(client)
+        previous_buckets = S3Monitor.get_buckets(client)
 
         while True:
             time.sleep(SCAN_INTERVAL)
             try:
-                buckets = get_buckets(client)
+                buckets = S3Monitor.get_buckets(client)
             except boto3.exceptions.botocore.errorfactory.ClientError:
                 buckets = None
 
             if buckets is not None:
-                created_buckets, deleted_buckets = compare_buckets(previous_buckets, buckets)
+                created_buckets, deleted_buckets = S3Monitor.compare_buckets(previous_buckets, buckets)
                 for bucket in created_buckets:
                     siddhi_input_handler.send(['BUCKET_CREATED', bucket[0]])
 
@@ -43,10 +43,10 @@ class S3Monitor():
 
                 previous_buckets = buckets
 
-            self.monitor_objects_in_bucket('logs')
+            self.monitor_objects_in_bucket(siddhi_input_handler, 'logs')
 
 
-    def monitor_objects_in_bucket(bucket):
+    def monitor_objects_in_bucket(self, siddhi_input_handler, bucket):
         client = config.get_client()
         if bucket not in self._cache:
             self._cache[bucket] = set()
